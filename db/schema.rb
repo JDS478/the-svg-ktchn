f0f75e25cb12844fcdd712e18ebb88a182066b4a
Joel Savage <joel28169@gmail.com>
2023-10-08 20:55:52 +0100

    Setup Product model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_08_141850) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_08_195456) do
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "price"
+    t.integer "quantity"
+    t.text "chefs_note"
+    t.integer "servings"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
